d947c8a783f6cf899ba1d3ae7a061ad23cbeca85
guokai <3045217274@qq.com>
2022-12-03 22:59:48 +0800

    only modelsAPI update

--- ElectronicCommerce/NetworkingMall/databaseApi.py
+++ ElectronicCommerce/NetworkingMall/databaseApi.py
@@ -1,59 +1,172 @@
-from . import models
-
+from .models import *
 
 def create_customer(customer_name, mail_address, password, phone_number):
-    # 在数据库中创建一个customer，返回bool值， 创建成功则返回True， 创建失败(mail_address重复或customer重复)返回False
-    return False
+    # 在数据库中创建一个customer，返回bool值， 创建成功则返回True， 创建失败(mail_address重复或customer重复)返回False  
+    try:
+        customer_set_1 = Customer.objects.filter(customerName = customer_name)
+        customer_set_2 = Customer.objects.filter(mailAddress = mail_address)
 
+        if customer_set_1.exists() and customer_set_2.exists():
+            '''print('用户名已被占用; 邮箱已注册账号')'''
+            return False
+        elif customer_set_1.exists():
+            '''print('用户名已被占用')'''
+            return False
+        elif customer_set_2.exists():
+            '''print('邮箱已注册账号')'''
+            return False
+        else:
+            Customer.objects.create(customerName = customer_name, mailAddress = mail_address,
+            password = password, phoneNumber = phone_number)
+            return True
+    except:
+        print('创建顾客用户异常')   
+        return False
 
 def create_seller(seller_name, mail_address, password, phone_number, shipping_address):
-    # 在数据库中创建一个seller, 返回bool值， 成功返回True， 失败返回False, 同样，检测mail_address，seller_name的重复性
-    return False
+    # 在数据库中创建一个seller, 返回bool值， 成功返回True， 用户名重复、邮箱重复或创建失败返回False 同样，检测mail_address，seller_name的重复性
+    try:
+        seller_set_1 = Seller.objects.filter(sellerName = seller_name)
+        seller_set_2 = Seller.objects.filter(mailAddress = mail_address)
 
+        if seller_set_1.exists() and seller_set_2.exists():
+            '''print('用户名已被占用; 邮箱已注册账号')'''
+            return False
+        elif seller_set_1.exists():
+            '''print('用户名已被占用')'''
+            return False
+        elif seller_set_2.exists():
+            '''print('邮箱已注册账号')'''
+            return False
+        else:
+            Seller.objects.create(sellerName = seller_name, mailAddress = mail_address,
+            password = password, phoneNumber = phone_number, shippingAddress = shipping_address)
+            return True
+    except:
+        print('创建商家用户异常')   
+        return False
 
 def create_goods(seller_id, goods_name, goods_stock, goods_price, goods_type):
-    # 在数据库goods表，创建一个新的商品
+    # 在数据库goods表，创建一个新的商品,创建成功返回goodsID，创建失败返回-1
     # 暂时先返回goods_id
-    return 1
-
+    try:
+        seller = Seller.objects.get(sellerID = seller_id)
+        goods = Goods.objects.create(goodsName = goods_name, goodsStock = goods_stock,goodsPrice = goods_price, goodsType = goods_type,sellerID = seller)
+        return goods.goodsID
+    except Seller.DoesNotExist:
+        print('seller_id_error:商家不存在')
+    except:
+        print('create_goods_error:添加新商品失败') # 创建商品失败
+        return -1
 
 def create_photo_path(goods_id, photo_path):
     # 在数据库Photos表中，插入一个元组，成功创建返回True,失败则返回False
-    return False
-
+    try:
+        goods = Goods.objects.get(goodsID = goods_id)
+        Photos.objects.create(goodsID = goods, photosPath = photo_path)
+    except Goods.DoesNotExist:
+        print('goods_id_error:商品不存在')
+        return False
+    except:        
+        return False
 
 def create_intended_goods(customer_id, goods_id, quantity):
-    # 意向商品表中插入一项
-    # 不需要返回值
-    return
-
-
+    # 意向商品表中插入一项 不需要返回值
+    try:
+        customer = Customer.objects.get(customerID = customer_id)
+        goods = Goods.objects.get(goodsID = goods_id)
+        IntendedGoods.objects.create(goodsID = goods, customerID = customer, quantity = quantity)
+    except Customer.DoesNotExist:  
+        print('customer_id_error:顾客不存在')    
+    except Goods.DoesNotExist:
+        print('goods_id_error:商品不存在')
+    except:
+        print('添加意向商品失败')   
+    
 def create_order(customer_id, goods_id, quantity, ship_to_address):
     # 订单表中创建一项order
     # amount, goodsName, customerName, sellerName,createTime 通过查找数据库计算得出
-    return
-
+    try:
+        customer = Customer.objects.get(customerID = customer_id)
+        goods = Goods.objects.get(goodsID = goods_id)
+        seller = Seller.objects.filter(goods__goodsID = goods_id)
+        
+        Order.objects.create(amount = goods.goodsPrice * quantity, 
+                            goodsName = goods.goodsName,
+                            goodsQuantity = quantity,
+                            customerName = customer.customerName,
+                            sellerName = seller[0].sellerName,        
+                            shipTpAddress = ship_to_address,
+                            customerID = customer 
+                            )
+    except Customer.DoesNotExist:
+        print('customer_id_error:顾客不存在')
+    except Goods.DoesNotExist:
+        print('goods_id_error:商品不存在')
+    except:
+        print('创建订单失败')
 
 def create_comment(customer_id, goods_id, comment):
     # 向数据库中插入一条评论
-    return
-
+    try:
+        customer = Customer.objects.get(customerID = customer_id)
+        goods = Goods.objects.get(goodsID = goods_id)
+        Comment.objects.create(goodsID = goods, customerID = customer, comment=comment)
+    except Customer.DoesNotExist:  
+        print('customer_id_error:顾客不存在')    
+    except Goods.DoesNotExist:
+        print('goods_id_error:商品不存在')
+    except:
+        print('添加评论失败')   
 
 def customer_login_by_name(name, password):
-    # 返回布尔值
-    return False
-
-
+    # 登陆成功返回id，否则返回0
+    user = Customer.objects.filter(customerName = name)
+    if user.exists() == False:
+        print('登录失败，用户名不存在')
+        return 0
+    else:
+        if user.get().password == password:
+            return user.get().customerID
+        else:
+            print('登录失败，密码错误')
+            return 0
+    
 def customer_login_by_mail_address(mail_address, password):
-    # 返回布尔值
-    return False
-
+    # 登陆成功返回id，否则返回0
+    user = Customer.objects.filter(mailAddress = mail_address)
+    if user.exists() == False:
+        print('登录失败，用户邮箱不存在')
+        return 0
+    else:
+        if user.get().password == password:
+            return user.get().customerID
+        else:
+            print('登录失败，密码错误')
+            return 0
 
 def seller_login_by_mail_address(mail_address, password):
-    # 返回布尔值
-    return False
-
+    # 登陆成功返回id，否则返回0
+    user = Seller.objects.filter(mailAddress = mail_address)
+    if user.exists() == False:
+        print('登录失败，用户邮箱不存在')
+        return 0
+    else:
+        if user.get().password == password:
+            return user.get().sellerID
+        else:
+            print('登录失败，密码错误')
+            return 0
 
 def seller_login_by_name(name, password):
-    # 返回布尔值
-    return False
+    # 登陆成功返回id，否则返回0
+    user = Seller.objects.filter(sellerName = name)
+    if user.exists() == False:
+        print('登录失败，用户名不存在')
+        return 0
+    else:
+        if user.get().password == password:
+            return user.get().sellerID
+        else:
+            print('登录失败，密码错误')
+            return 0
--- ElectronicCommerce/NetworkingMall/models.py
+++ ElectronicCommerce/NetworkingMall/models.py
@@ -5,38 +5,41 @@ from django.db import models
 
 
 def images_path():
-    return os.path.join(settings.LOCAL_FILE_DIR, 'image')  # 需要重写
-
+    return os.path.join(settings.STATIC_URL, 'image')
 
 # 顾客信息模型
 class Customer(models.Model):
-    customerID = models.AutoField(primary_key=True)  # 主键 顾客用户ID IntegerField, 可以根据可用的ID自动递增
-    mailAddress = models.EmailField(unique=True)  # 邮件地址 CharField 使用EmailValidator来检查该值是否是有效的邮件地址,要求unique
+
+    # 主键 顾客用户ID IntegerField, 可以根据可用的ID自动递增
+    customerID = models.AutoField(primary_key=True)
+    # 邮件地址 CharField 使用EmailValidator来检查该值是否是有效的邮件地址
+    mailAddress = models.EmailField(unique=True)
     customerName = models.CharField(max_length=40, unique=True)  # 顾客用户名
     password = models.CharField(max_length=20)  # 密码
     phoneNumber = models.CharField(max_length=20)  # 手机号
 
     def __str__(self):
-        return "{} {} {} {} {}".format(self.customerID, self.mailAddress, self.customerName, self.password,
-                                       self.phoneNumber)
-
+        return "{} {} {} {} {}".format(self.customerID, self.mailAddress, self.customerName, self.password, self.phoneNumber)
 
 # 商家信息模型
+
+
 class Seller(models.Model):
+
     sellerID = models.AutoField(primary_key=True)  # 主键 商家用户ID IntegerField
-    mailAddress = models.EmailField()  # 邮件地址 CharField
+    mailAddress = models.EmailField(unique=True)  # 邮件地址 CharField
     sellerName = models.CharField(max_length=40, unique=True)  # 商家用户名
     password = models.CharField(max_length=20)  # 密码
     phoneNumber = models.CharField(max_length=20)  # 手机号
     shippingAddress = models.CharField(max_length=80)  # 发货地址
 
     def __str__(self):
-        return "{} {} {} {} {} {}".format(self.sellerID, self.mailAddress, self.sellerName, self.password,
-                                          self.phoneNumber, self.shippingAddress)
+        return "{} {} {} {} {} {}".format(self.sellerID, self.mailAddress, self.sellerName, self.password, self.phoneNumber, self.shippingAddress)
 
 
 # 商品信息模型
 class Goods(models.Model):
+
     goodtype = [
         ('1', '女装'), ('2', '男装'), ('3', '食品'), ('4', '医药'),  # 后期根据需求再添加选项
     ]
@@ -44,7 +47,8 @@ class Goods(models.Model):
     goodsID = models.AutoField(primary_key=True)  # 主键 商品ID IntegerField
     goodsName = models.CharField(max_length=60)  # 商品名
     goodsStock = models.IntegerField()  # 商品库存量
-    goodsPrice = models.DecimalField(max_digits=12, decimal_places=2)  # 商品价格 使用NumberInput表单部件
+    goodsPrice = models.DecimalField(
+        max_digits=12, decimal_pglaces=2)  # 商品价格 使用TextInput表单部件
     # 以两位小数的精度来存储整数位有10位的数字 ； 以DecimalValidator来验证输入是否是固定精度的十进制
     goodsType = models.CharField(max_length=20, choices=goodtype)  # 商品种类
     # 在表单中以选择框的形式呈现
@@ -52,8 +56,9 @@ class Goods(models.Model):
     sellerID = models.ForeignKey(
         'Seller',
         on_delete=models.CASCADE,
-    )  # 外码 商家ID 商家和商品之间是一对多关系
-    commentedCustomer = models.ManyToManyField(Customer, through='Comment')  # 商品和顾客之间通过中间模型Comment关联起来
+    )   # 外码 商家ID 商家和商品之间是一对多关系
+    commentedCustomer = models.ManyToManyField(
+        Customer, through='Comment')  # 商品和顾客之间通过中间模型Comment关联起来
 
     def __str__(self):
         return "{} {} 库存{}件 {}RMB {} 卖家ID:{}".format(self.goodsID, self.goodsName,
@@ -62,19 +67,21 @@ class Goods(models.Model):
 
 # 商品照片模型
 class Photos(models.Model):
+
     photosID = models.AutoField(primary_key=True)  # 主码 图片ID
     photosPath = models.FilePathField(path=images_path, unique=True)  # 图片路径
     # goodsID仍需更完善的定义
     goodsID = models.ForeignKey(
         'Goods',
         on_delete=models.CASCADE,
-    )  # 外码 商品ID 商品和图片之间是一对多关系
+    )   # 外码 商品ID 商家和图片之间是一对多关系
 
     def __str__(self):
         return "{} {} 商品ID{}".format(self.photosID, self.photosPath, self.goodsID)
 
-
 # 意向商品模型
+
+
 class IntendedGoods(models.Model):
     goodsID = models.ForeignKey(
         'Goods',
@@ -92,13 +99,14 @@ class IntendedGoods(models.Model):
 
 # 订单信息模型
 class Order(models.Model):
-    orderID = models.AutoField(primary_key=True)  # 主码 订单ID
+    orderID = models.AutoField(primary_key=True)  # 主码 商品ID
     amount = models.DecimalField(max_digits=12, decimal_places=2)  # 订单价格
     goodsName = models.CharField(max_length=60)  # 商品名
     goodsQuantity = models.IntegerField()  # 商品数量
     customerName = models.CharField(max_length=40, unique=True)  # 顾客用户名
     sellerName = models.CharField(max_length=40, unique=True)  # 商家用户名
-    createTime = models.DateTimeField(auto_now=False, auto_now_add=True)  # 创建时间
+    createTime = models.DateTimeField(
+        auto_now=False, auto_now_add=True)  # 创建时间
     shipToAddress = models.CharField(max_length=80)  # 收货地址
     customerID = models.ForeignKey(
         'Customer',
@@ -106,17 +114,15 @@ class Order(models.Model):
     )  # 外码 顾客ID
 
     def __str__(self):
-        return "商品ID{} {}RMB {} {}件 顾客:{} 商家:{} {} 收货地址{}".format(self.orderID, self.amount, self.goodsName,
-                                                                  self.goodsQuantity,
-                                                                  self.customerName, self.sellerName, self.createTime,
-                                                                  self.shipToAddress)
+        return "商品ID{} {}RMB {} {}件 顾客:{} 商家:{} {} 收货地址{}".format(self.orderID, self.amount, self.goodsName, self.goodsQuantity,
+                                                                  self.customerName, self.sellerName, self.createTime, self.shipToAddress)
 
     # 判断此订单在某一时间间隔内是否可以取消
-    def cancel(self, interval=4320):  # 默认三天内可取消
-        now_time = datetime.datetime.now()
-        create_time = datetime.strptime(self.createTime, "%Y-%m-%d %H:%M:%S")
+    def cancel(self, interval=4320):  # 默认三天内可取消,暂时以秒为单位
+        nowtime = datetime.datetime.now()
+        createTime = datetime.datetime.strptime(self.createTime, "%Y-%m-%d %H:%M:%S")
 
-        if (now_time - create_time).total_seconds <= interval:
+        if (nowtime-createTime).total_seconds <= interval:
             return True
         else:
             return False
